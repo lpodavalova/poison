2dd661ff5815346e692831cba53950577b9914e8
Anton Podavalov <a.podavalov@gmail.com>
2014-04-27 18:55:46 +0400

    Fixed copyright years.

---
+++ Poison.Test/Modelling/TestModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Poison.Modelling;
+using Poison.Stochastic;
+
+namespace Poison.Test.Modelling
+{
+    class TestModel : Model
+    {
+        private const string queue1 = "queue1";
+        private const string generator1 = "generator1";
+        private const string facility1 = "facility1";
+
+        private int transactionCount = 10000000;
+        private Normal facilitySeizeTime = new Normal(6, 3);
+
+        protected override bool IsAlive()
+        {
+            return transactionCount >= 0;
+        }
+
+        public TestModel()
+        {
+            Queues.Add(new Queue(queue1));
+            Facilities.Add(new Facility(facility1));
+            Generators.Add(new Generator(generator1, new Normal(10, 0.5)));
+
+            Generators[generator1].Entered += TestModel_Entered;
+            Queues[queue1].Enqueued += TestModel_Enqueued;
+            Facilities[facility1].Released += TestModel_Released;
+        }
+
+        private void TestModel_Entered(Transact transact)
+        {
+            Queues[queue1].Enqueue(transact);
+        }
+
+        private void TestModel_Enqueued(Queue obj)
+        {
+            if (Facilities[facility1].State == FacilityState.Free)
+            {
+                SeizeFacility(Facilities[facility1], Queues[queue1].Dequeue());
+            }
+        }
+
+        private void TestModel_Released(Facility obj)
+        {
+            if (!Queues[queue1].Empty)
+            {
+                SeizeFacility(Facilities[facility1], Queues[queue1].Dequeue());
+            }
+
+            transactionCount--;
+        }
+
+        private void SeizeFacility(Facility facility, Transact transact)
+        {
+            facility.Seize(transact);
+            Advance(Math.Abs(facilitySeizeTime.Next()), ReleaseFacility);
+        }
+
+        private void ReleaseFacility()
+        {
+            Facilities[facility1].Release();
+        }
+    }
+}
